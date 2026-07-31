afd1b8b0ad812d06f9db0408dafcb266a3d20f13
Mufincandyangel <121390741+Mufincandyangel@users.noreply.github.com>
2026-07-31 16:22:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,58 @@ let level = 1;
 let coins = 0;
 
 
+function chooseStyle(style){
+
+let name =
+document.getElementById("name").value;
+
+
+if(name === ""){
+name="Player";
+}
+
+
+document.getElementById("playerName").innerHTML=name;
+
+
+document.getElementById("style").innerHTML=style;
+
+
+document.body.className=style;
+
+
+document.getElementById("creator").style.display="none";
+
+document.getElementById("game").style.display="block";
+
+
+}
+
+
+
 function completeQuest(amount){
 
-    xp += amount;
-    coins += 5;
+xp += amount;
+coins += 5;
 
 
-    if(xp >= 100){
+if(xp >=100){
 
-        level++;
-        xp = xp - 100;
+level++;
+xp=0;
 
-        alert("🎉 Level Up! You are now Level " + level);
-    }
+alert("🎉 Level Up!");
 
-
-    document.getElementById("xp").innerHTML = xp;
-    document.getElementById("level").innerHTML = level;
-    document.getElementById("coins").innerHTML = coins;
+}
 
 
-    document.getElementById("xp-bar").style.width = xp + "%";
+document.getElementById("xp").innerHTML=xp;
+
+document.getElementById("level").innerHTML=level;
+
+document.getElementById("coins").innerHTML=coins;
+
+
+document.getElementById("xp-bar").style.width=xp+"%";
 
 }
