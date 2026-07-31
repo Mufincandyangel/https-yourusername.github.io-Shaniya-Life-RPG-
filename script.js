f77f6a0bab00a52e1980bb677495bb7b66da2735
Mufincandyangel <121390741+Mufincandyangel@users.noreply.github.com>
2026-07-31 16:19:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let xp = 0;
+let level = 1;
+let coins = 0;
 
+
+function completeQuest(amount){
+
+    xp += amount;
+    coins += 5;
+
+
+    if(xp >= 100){
+
+        level++;
+        xp = xp - 100;
+
+        alert("🎉 Level Up! You are now Level " + level);
+    }
+
+
+    document.getElementById("xp").innerHTML = xp;
+    document.getElementById("level").innerHTML = level;
+    document.getElementById("coins").innerHTML = coins;
+
+
+    document.getElementById("xp-bar").style.width = xp + "%";
+
+}
